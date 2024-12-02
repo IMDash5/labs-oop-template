52376a1d65fce50a6e6bdf3a5778f8b6daf51121
imdash <mmavrin80@gmail.com>
2024-12-02 22:42:48 +0000

    lab5 no tests

--- main.cpp
+++ main.cpp
@@ -1,5 +1,36 @@
+#include "include/listMemoryResource.h"
+#include "include/DynamicArray.h"
 #include <iostream>
+#include <string>
 
-#include "lib.h"
+struct ComplexType {
+    int id;
+    std::string name;
 
-int main(){}
+    ComplexType(int id, const std::string& name)
+        : id(id), name(name) {}
+};
+
+int main() {
+    ListMemoryResource memory_resource;
+
+    DynamicArray<int> int_array{&memory_resource};
+    int_array.push_back(1);
+    int_array.push_back(2);
+    int_array.push_back(3);
+
+    for (const auto& val : int_array) {
+        std::cout << val << " ";
+    }
+    std::cout << std::endl;
+
+    DynamicArray<ComplexType> complex_array{&memory_resource};
+    complex_array.push_back({1, "Alice"});
+    complex_array.push_back({2, "Bob"});
+
+    for (const auto& obj : complex_array) {
+        std::cout << obj.id << ": " << obj.name << std::endl;
+    }
+
+    return 0;
+}
